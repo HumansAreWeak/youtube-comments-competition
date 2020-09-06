d1791bb9550dc70da3548c9183c9d06310507749
Maik Steiger <fifeimcity@gmail.com>
2020-09-06 17:07:46 +0200

    Initial Commit

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict";
 module.exports = function (app, opts) {
-  // Setup routes, middleware, and handlers
-  app.get('/', (req, res) => {
-    res.locals.name = '.'
-    res.render('index')
-  })
-}
+    // Setup routes, middleware, and handlers
+    app.get("/", (req, res) => {
+        res.locals.name = "YouTube Comments Competition";
+        res.render("index");
+    });
+};
