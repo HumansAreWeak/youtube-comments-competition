f9710e7caae1831be10b7fc37b983ffb49d836f0
Maik Steiger <fifeimcity@gmail.com>
2020-09-07 20:14:18 +0200

    Update Server

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 const publicPath = path.join(__dirname, "public");
 
 const app = express();
+app.set("port", PORT);
+app.set("host", "localhost");
 app.set("view engine", "ejs");
 app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: false }));
